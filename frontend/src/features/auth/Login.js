5bc1786163704e8d1851ff67ac2aba1e2072f22d
Michael Pacleb <michael.pacleb@gmail.com>
2023-02-02 22:50:44 -0500

    added Login component.

--- frontend/src/features/auth/Login.js
+++ frontend/src/features/auth/Login.js
@@ -1,7 +1,114 @@
-import React from "react";
+import { useRef, useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setCredentials } from "./authSlice";
+import { useLoginMutation } from "./authApiSlice";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [username, password]);
+
+  // submit handler
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { accessToken } = await login({ username, password }).unwrap();
+      dispatch(setCredentials({ accessToken }));
+
+      // clear input components
+      setUsername("");
+      setPassword("");
+
+      navigate("/dash");
+    } catch (err) {
+      if (!err.status) {
+        setErrMsg("No Server Response");
+      } else if (err.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg(err.data?.message);
+      }
+
+      errRef.current.focus();
+    }
+  };
+
+  const errClass = errMsg ? "errmsg" : "offscreen";
+
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  content = (
+    <section className="public">
+      <header>
+        <h1>Employee Login</h1>
+      </header>
+
+      <main className="login">
+        <p ref={errRef} className={errClass} aria-live="assertive">
+          {errMsg}
+        </p>
+
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+
+          <input
+            type="text"
+            className="form__input"
+            ref={userRef}
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            autoComplete="off"
+            required
+          />
+
+          <label htmlFor="password">Password:</label>
+
+          <input
+            type="password"
+            className="form__input"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button className="form__submit-button">Sign In</button>
+        </form>
+      </main>
+
+      <footer>
+        <Link to="/">Back to Home</Link>
+      </footer>
+    </section>
+  );
+
+  return content;
 };
 
 export default Login;
